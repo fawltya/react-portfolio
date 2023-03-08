1f27c3134ba644349b470f9e3467df5ebb56e62a
Seamus Barnett <seamus0689@gmail.com>
2023-03-08 21:31:25 +0000

    Button styling fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
     fluidTypography: {},
     extend: {
       fontFamily: {
-        exensaBlack: ["ExensaGrotesk-Black", "sans-serif"],
-        exensaBold: ["ExensaGrotesk-Bold", "sans-serif"],
-        exensaRegular: ["ExensaGrotesk-Regular", "sans-serif"],
         retroChimps: ["retro-chimps", "sans-serif"],
         caSpotnik: ["ca-spotnik", "sans-serif"],
         caSpotnikBold: ["ca-spotnik-bold", "sans-serif"],
